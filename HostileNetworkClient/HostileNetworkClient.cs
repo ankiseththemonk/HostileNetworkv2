4636af6848a0d130faa631a996a4a34fa1329e07
Stephen Willson <stephendwillson@live.com>
2013-10-25 15:22:36 -0400

    Initial commit.
    Initial commit of client, server, and utils projects.

---
+++ HostileNetworkClient/HostileNetworkClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using HostileNetworkUtils;
+using System.Net.Sockets;
+using System.Net;
+
+namespace HostileNetwork {
+    class HostileNetworkClient {
+
+        static void Main() {
+
+            string command = acceptCommand();
+            string fileName = "";
+
+            switch (command) {
+                case "send":
+                    fileName = getValidFileName();
+                    break;
+                case "get":
+                    break;
+                case "dir":
+                    Console.WriteLine("Requesting directory listing from server...");
+                    break;
+                default:
+                    break;
+            }
+
+
+            const int PORT = 58008;
+            string data = "";
+            byte[] sendBytes = new Byte[1024];
+            byte[] rcvPacket = new Byte[1024];
+            UdpClient client = new UdpClient();
+            IPAddress address = IPAddress.Parse(IPAddress.Broadcast.ToString());
+            client.Connect(address, PORT);
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Client is Started");
+            Console.WriteLine("Type your message");
+
+            while (data != "q") {
+                data = Console.ReadLine();
+                sendBytes = Encoding.ASCII.GetBytes(DateTime.Now.ToString() + " " + data);
+                client.Send(sendBytes, sendBytes.GetLength(0));
+                rcvPacket = client.Receive(ref remoteIPEndPoint);
+
+                string rcvData = Encoding.ASCII.GetString(rcvPacket);
+                Console.WriteLine("Handling client at " + remoteIPEndPoint + " - ");
+
+                Console.WriteLine("Message Received: " + rcvPacket.ToString());
+            }
+            Console.WriteLine("Close Port Command Sent");  //user feedback
+            Console.ReadLine();
+            client.Close();  //close connection
+        }
+
+        static string getValidFileName() {
+
+            Console.Write("Please enter a filepath\\filename: ");
+            string fileName = Console.ReadLine();
+
+            while (fileName == "" || !Utils.fileExists(fileName)) {
+                Console.WriteLine("Invalid file name or file does not exist.\n");
+                Console.Write("Please enter a filepath\\filename: ");
+                fileName = Console.ReadLine();
+            }
+            return fileName;
+        }
+
+        static string acceptCommand() {
+
+            string command = "";
+
+            Console.WriteLine("Usage: <get|send|dir>");
+            Console.Write("Please enter a command: ");
+            command = Console.ReadLine();
+
+            while (command != "dir" && command != "get" && command != "send") {
+                Console.WriteLine("Invalid command. Usage: <get|send|dir>\n");
+                Console.Write("Please enter a command: ");
+                command = Console.ReadLine();
+            }
+
+            return command;
+        }
+    }
+}
